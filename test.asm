67cac609384d7779a6c31cdef0e9915c032b4cf6
Uko1ove <a.ukolov@gmail.com>
2020-12-21 03:55:55 +0300

    multiply numbers unsigned

--- test.asm
+++ test.asm
@@ -8,73 +8,45 @@ extern ExitProcess@4:near
 
 
 _DATA SEGMENT
-    ExitCode dd 0
-    dwNum_1 dd 0
-    dwNum_2 dd 0
-    dwResult dd 0
-
+    ExitCode    dd 0
+    a           db 60
+    b           db 9
+    
 _DATA ENDS
 
 
 _TEXT SEGMENT
 
 START:
-    mov dwNum_1,5
-    mov dwNum_2,10
+    mov eax,0
+    mov ebx,0
+    mov al,a
+    mov bl,b
+    push ebx
+    push eax
+    call MulProc
 
-    push dwNum_2
-    push dwNum_1
-    call AddProc
-    mov dwResult,eax    ;---or mov dword ptr[dwResult],eax----
 
-    mov ebx,256
-    mov edx,-127        ;--mov args to function
-    push _next
-    push offset SubProc     ;--push function address on the top of stack
-    ret                     ;--jump to the adderss of function
-    
-_next:                  ;--create mark for next row
     push [ExitCode]
     call ExitProcess@4 
 ;--------------------------
-AddProc proc
-    push ebp                    ;--первые 2 сторчки можно заменить на enter 0,0
-    mov ebp,esp
-    ;------------------
-    sub esp, 8                  ;--выделили место в стэке дл€ 2 локальных переменных
-    ;------------------
-    mov dword ptr[ebp-4],0
-                                ;--объ€вление локальных переменных и их обнуление
-    mov dword ptr[ebp-8],0
-    ;------------------
-    
+MulProc proc
+    enter 0,0                   ;-- = push ebp  mov ebp,esp
     push ebx
     push esi
     push edi
-;--------------------------
-    mov eax,dword ptr[ebp+8]
-    mov ebx,dword ptr[ebp+0ch]
-    add eax,ebx
+;-----------------------
+    mov eax,0                   ;-- mutl and '/' use only eax
+    mov al,byte ptr[ebp+8]      ;-- move to junior byte eax
+    mov bl,byte ptr[ebp+12]     ;-- move to junior byte ebx
+    mul bl                      ;-- multiply al*bl
 
-;---------------------------
+;-----------------------
     pop edi
     pop esi
     pop ebx
-    mov esp,ebp                  ;-- 2 последние перед ret можно замениеть на leave
-    pop ebp
-    ret 8
-AddProc endp
-;--------------------------
-
-SubProc proc
-    sub ebx,edx                  ;--function operation with arguments
-    neg ebx
-    not ebx
-    inc ebx
-    dec ebx
-    mov eax,ebx
-    ret
-SubProc endp
+    
+MulProc endp
 
 _TEXT ENDS
 END START
