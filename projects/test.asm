85ba3176b535c1d01081ad58e5b77125bcb54474
Uko1ove <a.ukolov@gmail.com>
2021-01-23 00:17:59 +0300

    output of the result of 'small' function with format

--- projects/test.asm
+++ projects/test.asm
@@ -2,20 +2,26 @@
 .model flat, stdcall
 ;---------------------
 includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\user32.lib
 extern ExitProcess@4:near
 extern GetStdHandle@4:near                     ;-function
 extern CloseHandle@4:near
 extern lstrlenA@4:near
 extern WriteConsoleA@20:near
 extern ReadConsoleA@20:near
+extern wsprintfA:near                   ;- it has indefined num of parameters
 ;---------------------
 _DATA SEGMENT
     ExitCode    dd 0
-    hIn         dd 0                        ;var for input
-    hOut        dd 0                        ;var for output
-    szExit      db "Press any key...",0     ;--string
-    dwCnt       dd 0                        ;var for cout of symbols
-    szBuffer    db 255 dup(0)               
+    hIn         dd 0                                ;var for input
+    hOut        dd 0                                ;var for output
+    szExit      db 13,10,"Press any key...",0       ;--string
+    dwCnt       dd 0                                ;var for cout of symbols
+    szBuffer    db 255 dup(0)
+    szFrm       db "(2ac - bx - 12) / (cx + a) = %d",13,10,0    ;string with format
+                                            ; format begins with "%". %d is format
+                                            ; 13,10,0 new str, return carett
+    szBuffer2   db 32 dup(0)              
         
 _DATA ENDS
 
@@ -31,7 +37,33 @@ START:
     push -11                    ;--output stream
     call GetStdHandle@4
     mov hOut,eax
-    
+;------------------------
+    push 4
+    push -5
+    push 16
+    push 3
+    call small
+;------------------------
+    push eax                    ;-after small proc the result is in eax
+    lea eax,szFrm               ;-get address of format and move it to eax
+    push eax                    ;--push the address of format to stack
+    lea eax,szBuffer2           ;--get the address of buffer2
+    push eax                    ;--push buffer2 address to stack
+    call wsprintfA              ;--transform the result of "call" to string and
+                                ;--write the result into buffer2
+;-----------------------
+    add esp,12                  ;--cleane stack, cause it's C function
+;-----------------------
+    push offset szBuffer2       ;--transfer the buffer2 address to function
+    call lstrlenA@4             ;--get the string length and put it into eax
+;-----------------------
+    push 0
+    push offset dwCnt           ;-var for number of written symbols
+    push eax                    ;-string length
+    push offset szBuffer2       ;--string. Address
+    push hOut                   ;--handle output
+    call WriteConsoleA@20
+;------------------------
     lea eax,szExit              ;--new command lea. Puts address in register
     push eax
     call lstrlenA@4
@@ -52,11 +84,7 @@ START:
     push eax                    ;--var where buffer is written
     push[hIn]                   ;--handle (in this case input)
     call ReadConsoleA@20
-
-
-
-
-    call small
+;-------------------------
     
     push hIn
     call CloseHandle@4          ;--close and clean
