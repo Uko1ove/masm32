c68ae93484acf694a5370e1076e91ddb14e15462
Uko1ove <a.ukolov@gmail.com>
2021-01-26 01:38:45 +0300

    console app template with loops, arrays, indexes, marks and input and output

--- projects/test.asm
+++ projects/test.asm
@@ -1,28 +1,25 @@
-.586P
+.686P
 .model flat, stdcall
 ;---------------------
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\user32.lib
 extern ExitProcess@4:near
-extern GetStdHandle@4:near                     ;-function
+extern GetStdHandle@4:near                     
 extern CloseHandle@4:near
-extern lstrlenA@4:near
 extern WriteConsoleA@20:near
 extern ReadConsoleA@20:near
-extern wsprintfA:near                   ;- it has indefined num of parameters
+extern wsprintfA:near                   
 ;---------------------
+MAX_SIZE        equ     255
+
+
+
 _DATA SEGMENT
     ExitCode    dd 0
-    hIn         dd 0                                ;var for input
-    hOut        dd 0                                ;var for output
-    szExit      db 13,10,"Press any key...",0       ;--string
-    dwCnt       dd 0                                ;var for cout of symbols
-    szBuffer    db 255 dup(0)
-    szFrm       db "(2ac - bx - 12) / (cx + a) = %d",13,10,0    ;string with format
-                                            ; format begins with "%". %d is format
-                                            ; 13,10,0 new str, return carett
-    szBuffer2   db 32 dup(0)              
-        
+    szIntro     db 13,10,"Press <q> to quite, or <a> to execute small",13,10,0
+    szBuffer    db MAX_SIZE dup(0)
+    szError     db "Input error! Please, try again.",13,10,0
+    szSmall     db 07,"Hello! Here I am :)",13,10,0
 _DATA ENDS
 
 
@@ -30,110 +27,174 @@ _TEXT SEGMENT
 
 START:
 
-    push -10                    ;--input stream
-    call GetStdHandle@4
-    mov hIn,eax                 ;--move handle from eax to var
 
-    push -11                    ;--output stream
-    call GetStdHandle@4
-    mov hOut,eax
-;------------------------
-    push 4
-    push -5
-    push 16
-    push 3
-    call small
-;------------------------
-    push eax                    ;-after small proc the result is in eax
-    lea eax,szFrm               ;-get address of format and move it to eax
-    push eax                    ;--push the address of format to stack
-    lea eax,szBuffer2           ;--get the address of buffer2
-    push eax                    ;--push buffer2 address to stack
-    call wsprintfA              ;--transform the result of "call" to string and
-                                ;--write the result into buffer2
-;-----------------------
-    add esp,12                  ;--cleane stack, cause it's C function
-;-----------------------
-    push offset szBuffer2       ;--transfer the buffer2 address to function
-    call lstrlenA@4             ;--get the string length and put it into eax
-;-----------------------
-    push 0
-    push offset dwCnt           ;-var for number of written symbols
-    push eax                    ;-string length
-    push offset szBuffer2       ;--string. Address
-    push hOut                   ;--handle output
-    call WriteConsoleA@20
-;------------------------
-    lea eax,szExit              ;--new command lea. Puts address in register
-    push eax
-    call lstrlenA@4
+    call main
 ;-------------------------
-    push 0                      ;--push args from end to beginning <--NULL
-    lea ebx,dwCnt 
-    push ebx                    ;--variable to put count of symbols
-    push eax                    ;--strlen--is written to eax-look before call
-    push offset szExit          ;--string
-    push [hOut]                 ;--handle--(in this case output)
-    call WriteConsoleA@20
-;-------------------------
-    push 0                      ;--NULL
-    lea ebx,dwCnt
-    push ebx                    ;--var (addres where we put the count of symbols)
-    push 255                    ;--buffer size
-    lea eax,szBuffer
-    push eax                    ;--var where buffer is written
-    push[hIn]                   ;--handle (in this case input)
-    call ReadConsoleA@20
-;-------------------------
-    
-    push hIn
-    call CloseHandle@4          ;--close and clean
-
-    push hOut
-    call CloseHandle@4
-    
-    
     push [ExitCode]
     call ExitProcess@4
-;--------------------------
-small proc                      
+;**************************************
+main proc                                   ;--function main
+
+    _do:
+        push offset szIntro
+        call cout
+        ;-------------------
+        _while_:
+            push offset szBuffer
+            call cin
+            ;---------------
+            mov al,byte ptr[szBuffer]
+            ;----------------
+            cmp al,'q'                      ;-!INMPORTANT!-only '' quotes, not " ". OR 71h
+            je _do_end
+            ;----------------
+            cmp al,61h                      ;---'a'--
+            je _small
+            ;----------------
+            push offset szError
+            call cout
+            ;----------------
+            jmp _while_
+
+            _small:
+                call small
+                jmp _while_
+        _while__end:
+
+
+    _do_end:
+
+
+
+    ret
+main endp
+;***************************************
+;ebp+8 - pStr - string pointer 
+
+cout proc                                   ;--function of organization of output
+    push ebp
+    mov ebp,esp
+    add esp,-8                              ;-create 2 local var for descriptor and 
+    ;-----------                                num of written symbols
+    push ebx
+    push esi
+    push edi
+;---------------------
+    push -11
+    call GetStdHandle@4
+    mov dword ptr[ebp-4],eax                ;--move handle from eax to local var 
+    ;-------------
+    mov esi,dword ptr[ebp+8]
+    push esi
+    call lens
+    ;-------------
+    push 0
+    mov ebx,ebp                             ;cause we can't just push this address
+    sub ebx,8
+    ;-------------
+    push ebx                                ;--var for number of written symbols
+    push eax                                ;--string length saved from lens function
+    push esi                                ;--string address
+    push dword ptr[ebp-4]                   ;--handle
+    call WriteConsoleA@20
+    
+;---------------------
+    pop edi
+    pop esi
+    pop ebx
+    mov esp,ebp
+    pop ebp
+    
+    ret 4
+cout endp
+;***************************************
+cin proc                                        ;--;function of organization of input
+    push ebp
+    mov ebp,esp
+    add esp,-4                                  ;--only 1 var. We don't create var for
+                                                ;handle, cause we will not use eax in
+                                                ;procedures within. For num of entered sym
+    ;---------------
+    push ebx
+    push esi
+    push edi
+;--------------------
+    mov edi,dword ptr[ebp+8]
+    ;----------------
+    push -10
+    call GetStdHandle@4
+    ;----------------
+    mov esi,ebp
+    sub esi,4                                   ;--address of local var
+    ;----------------
+    push 0
+    push esi                                    ;--string length
+    push 255
+    push edi                                    ;--buffer address
+    push eax                                    ;--handle
+    call ReadConsoleA@20
+    ;-----------------
+    sub dword ptr[esi],2                        ;--enter = +2 ASCII symbols: return carett
+                                                ;-- and new line.
+    mov esi,dword ptr[esi]
+    ;-----------------
+    mov byte ptr[edi+esi],0                     ;--example of index addressing
+;--------------------
+    pop edi
+    pop esi
+    pop ebx
+    mov esp,ebp
+    pop ebp
+    
+    ret 4
+cin endp
+;*****************************************
+lens proc                                   ;--function returns tring length to eax
+    push ebp
+    mov ebp,esp
+    push ebx
+    push esi
+    push edi
+;----------------
+    mov esi,dword ptr[ebp+8]                ;--address of string
+    ;------------
+    sub ecx,ecx
+    ;------------
+_while:                                     ;--we can give to it any name
+    mov al,byte ptr[esi]
+    cmp al,0
+    jz _end_while
+    ;-------------
+    inc ecx
+    inc esi
+    jmp _while
+    ;-------------
+_end_while:
+    mov eax,ecx
+;----------------
+    pop edi
+    pop esi
+    pop ebx
+    mov esp,ebp
+    pop ebp
+
+    ret 4
+lens endp
+;*******************************************
+small proc                                      ;--say hello and goodbye
     enter 0,0                   
     push ebx
     push esi
     push edi
 ;-----------------------
-    mov eax,dword ptr[ebp+8]
-    add eax,eax                             ;2a
-    imul dword ptr [ebp+16]                 ;2ac
-
-    mov ecx,eax
-
-    mov eax,dword ptr[ebp+12]
-    cdq
-    idiv dword ptr[ebp+20]                  ;b/x
-
-    sub ecx,eax
-    sub ecx,12
-
-    mov eax,dword ptr[ebp+16]
-    imul dword ptr[ebp+20]                  ;cx
-    mov ebx,eax
-    mov eax,dword ptr[ebp+8]
-    add ebx,eax                             ;cx+a
-;-----------------------
-    mov eax,ecx
-    cdq
-    idiv ebx
-    
-    
-    
-    
+    push offset szSmall
+    call cout
 ;-----------------------
     pop edi
     pop esi
     pop ebx
     leave
-    ret 16                           
+    ret                           
 small endp
 ;-------------------------
 _TEXT ENDS
